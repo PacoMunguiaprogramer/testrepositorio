f500a6528c12f222b057dede720b81af4426b6f0
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2024-03-18 23:10:58 -0600

    Added Tailwind and refactored Project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.html"],
+    theme: {
+      extend: {
+        colors: {
+            "nice-blue": "#d1eef6",
+            "passionate-blue": "#2a58fd"
+        }
+      },
+    },
+    plugins: [],
+  }
+
